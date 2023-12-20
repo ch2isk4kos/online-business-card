2f0f2cbd9f6b5b2173f37eff576bc1b496732c98
ch2isk4kos <ch2isk4kos@gmail.com>
2023-12-20 14:30:11 -0500

    import/config fontawesome icon(s)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,20 @@
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faAddressCard,
+  faHouse,
+  faLaptopCode,
+  faPaperPlane,
+} from "@fortawesome/free-solid-svg-icons";
+library.add(faHouse, faAddressCard, faLaptopCode, faPaperPlane);
 import "./App.css";
 
 const App = () => {
   return (
     <div className="App">
-      <nav>
-        <a href="">
-          <i></i>
-        </a>
-      </nav>
+      <Navbar />
+      <Main />
     </div>
   );
 };
